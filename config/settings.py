32b17eb8f3807fe287a13d0e1f0c3b583bc7875c
muratjumashev <jumasheff@gmail.com>
2021-06-05 18:50:11 +0600

    Read settings from env file

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&xtb)1r)bvau-tbi)t0&6x66*g1th9x3i540*hd#$re66&w(7m'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
+# Telergam bot credentials
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_USERNAME = os.environ.get('BOT_USERNAME')
 
 # Application definition
 
--- spec_letters_bot/credentials.py
+++ spec_letters_bot/credentials.py
@@ -1,3 +1,5 @@
 # Do not remove these 2 lines:
-BOT_TOKEN = '1552826567:AAFqjuEc7H_JNHD4Uvy4tztbzeTU1euRNGM'
-APP_NAME = 'spec_letters_bot'
+from django.conf import settings
+
+BOT_TOKEN = settings.BOT_TOKEN
+APP_NAME = settings.BOT_USERNAME
